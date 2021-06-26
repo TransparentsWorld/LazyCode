71d1dfad7dc79afc9c044d081172a989152eaa7e
TransparentsWorld <954232592@qq.com>
2021-06-26 23:50:52 +0800

    OneBtnOpenFilebug修复和新功能增加 1、修复控制台出现乱码的问题，因为执行了错误的批处理命令，删除之后解决。但在cmd中无乱码，是pycharm的字符集设置的问题； 2、增加了程序的健壮性，检测用户输入是否错误。若错误则要求重新输入。

--- OfficeJob/src/OneBtnOpenFile/OneBtnOpenFile.py
+++ OfficeJob/src/OneBtnOpenFile/OneBtnOpenFile.py
@@ -1,16 +1,24 @@
 import os
 import configparser
 
-# 从配置文件读取批处理命令
+# 从配置文件读取批处理命令，并执行实现一键打开多个常见文件、文件夹、程序和网页的功能
+
 config = configparser.ConfigParser()
 config.read("config.ini", encoding="utf-8")
 
 print('配置文件中一键启动组如下所示：')
-print(config.sections())
+start_set = config.sections()
+print(start_set)
 file_collection = input('请输入需要一键启动的启动组:')
 print('\r')
 
-config_items: list = config.items(file_collection)
-for config_item in config_items:
-    for config in config_item:
-        os.system(config)
+# 检测输入是否正确，错误的话要求用户从选项中选择。
+while file_collection not in start_set:
+    print('启动组名称输入错误，请从下列选项中选择：')
+    print(start_set)
+    file_collection = input('请输入需要一键启动的启动组:')
+    print('\r')
+else:
+    config_items: list = config.items(file_collection)
+    for config_item in config_items:
+        os.system(config_item[1])
